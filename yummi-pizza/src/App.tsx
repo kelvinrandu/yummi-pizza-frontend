a6d757ef66cccfaecc32a112aeff3275f6a56305
kelvinrandu <randukelvin@gmail.com>
2020-05-05 14:37:10 +0300

    added mock server

--- yummi-pizza/src/App.tsx
+++ yummi-pizza/src/App.tsx
@@ -2,11 +2,62 @@ import React from 'react';
 import './App.css';
 import Product from './components/Product';
 
+
+
+const products = [
+
+  {
+    id: 1,
+    name: "Apple",
+    description: "Red apples",
+    img: "https://tachyons.io/img/avatar_1.jpg",
+    price: 100
+  },
+
+  {
+    id: 2,
+    name: "A Cat",
+    description: "CCO (Chief Cat Officer)",
+    img: "https://tachyons.io/img/avatar_1.jpg",
+    price: 100
+  },
+
+
+  {
+    id: 3,
+    name: "Milk",
+    description: "Milk for the lactose tollerent",
+    img: "https://tachyons.io/img/avatar_1.jpg",
+    price: 100
+  },
+
+  {
+    id: 4,
+    name: "Banana",
+    description: "I need those carbs",
+    img: "https://tachyons.io/img/avatar_1.jpg",
+    price: 100
+  },
+
+
+  {
+    id: 5,
+    name: "Orange",
+    description: "Orange are orange",
+    img: "https://tachyons.io/img/avatar_1.jpg",
+    price: 100
+  }
+
+];
+
 const App: React.FC = () => {
   return (
-    <div className="App">
-         <Product /> 
-    </div>
+    <main className="pa3 pa5-ns flex flex-wrap">
+        {
+          products.map(p => <Product key={p.id} {...p} />)
+        }
+
+    </main>
   );
 }
 
--- yummi-pizza/src/components/Product.tsx
+++ yummi-pizza/src/components/Product.tsx
@@ -1,22 +1,24 @@
 import React from 'react';
-import '../App.css';
 
-const Product: React.FC = () => {
+
+const Product: React.FC  = () => {
+
   return (
     <article className="br2 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw5 center">
     <img src="http://placekitten.com/g/600/300" className="db w-100 br2 br--top" alt="Photo of a kitten looking menacing."/>
     <div className="pa2 ph3-ns pb3-ns">
       <div className="dt w-100 mt1">
         <div className="dtc">
-          <h1 className="f5 f4-ns mv0">pizza</h1>
+          <h1 className="f5 f4-ns mv0">name</h1>
         </div>
         <div className="dtc tr">
-          <h2 className="f5 mv0">$1,000</h2>
+          <h2 className="f5 mv0">$price</h2>
         </div>
       </div>
       <p className="f6 lh-copy measure mt2 mid-gray">
-        crunchy to the last bite
+     description
       </p>
+      <a className="f6 link dim br2 ph3 pv2 mb2 dib white bg-hot-pink" href="#0">ADD</a>
     </div>
   </article>
   );
